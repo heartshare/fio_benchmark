e3591565fa4c7157b9d55f8e37558d8bea19360d
KaitoAkita <kaitokun07@icloud.com>
2019-02-24 02:14:41 +0900

    debug fix

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fio_benchmark
 新システムのストレージ検証用 fioのansible
+# 実行手順
+- ansible/hosts/hostをベンチマークを実行したいマシンのIPアドレスに変更
+- 下記を実行
+
+```
+ansible-playbook ansible/fio.yml -i ansible/hosts/host --private-key ~/.ssh/id_rsa
+```
--- fio_shell/sequential_write/benchmark.sh
+++ fio_shell/sequential_write/benchmark.sh
@@ -1,30 +1,30 @@
 for i in `seq -w 10`
 do
-fio ./sequential_write/myjob.fio >> /root/sequential_write/1k/randwe_kvm_1k_$i.txt
-sed -i "s/bs=1k/bs=10k/g" ./sequential_write/myjob.fio
+fio myjob.fio >> /root/sequential_write/1k/randwe_kvm_1k_$i.txt
+sed -i "s/bs=1k/bs=10k/g" myjob.fio
 rm -rf /mnt/purestorage1/Sequential-Write.0.0
 
-fio ./sequential_write/myjob.fio >> /root/sequential_write/10k/randwe_kvm_10k_$i.txt
-sed -i "s/bs=10k/bs=100k/g" ./sequential_write/myjob.fio
+fio myjob.fio >> /root/sequential_write/10k/randwe_kvm_10k_$i.txt
+sed -i "s/bs=10k/bs=100k/g" myjob.fio
 rm -rf /mnt/purestorage1/Sequential-Write.0.0
 
-fio ./sequential_write/myjob.fio >> /root/sequential_write/100k/randwe_kvm_100k_$i.txt
-sed -i "s/bs=100k/bs=1m/g" ./sequential_write/myjob.fio
+fio myjob.fio >> /root/sequential_write/100k/randwe_kvm_100k_$i.txt
+sed -i "s/bs=100k/bs=1m/g" myjob.fio
 rm -rf /mnt/purestorage1/Sequential-Write.0.0
 
-fio ./sequential_write/myjob.fio >> /root/sequential_write/1m/randwe_kvm_1m_$i.txt
-sed -i "s/bs=1m/bs=10m/g" ./sequential_write/myjob.fio
+fio myjob.fio >> /root/sequential_write/1m/randwe_kvm_1m_$i.txt
+sed -i "s/bs=1m/bs=10m/g" myjob.fio
 rm -rf /mnt/purestorage1/Sequential-Write.0.0
 
-fio ./sequential_write/myjob.fio >> /root/sequential_write/10m/randwe_kvm_10m_$i.txt
-sed -i "s/bs=10m/bs=100m/g" ./sequential_write/myjob.fio
+fio myjob.fio >> /root/sequential_write/10m/randwe_kvm_10m_$i.txt
+sed -i "s/bs=10m/bs=100m/g" myjob.fio
 rm -rf /mnt/purestorage1/Sequential-Write.0.0
 
-fio ./sequential_write/myjob.fio >> /root/sequential_write/100m/randwe_kvm_100m_$i.txt
-sed -i "s/bs=100m/bs=1g/g" ./sequential_write/myjob.fio
+fio myjob.fio >> /root/sequential_write/100m/randwe_kvm_100m_$i.txt
+sed -i "s/bs=100m/bs=1g/g" myjob.fio
 rm -rf /mnt/purestorage1/Sequential-Write.0.0
 
-fio ./sequential_write/myjob.fio >> /root/sequential_write/1g/randwe_kvm_1g_$i.txt
-sed -i "s/bs=1g/bs=1k/g" ./sequential_write/myjob.fio
+fio myjob.fio >> /root/sequential_write/1g/randwe_kvm_1g_$i.txt
+sed -i "s/bs=1g/bs=1k/g" myjob.fio
 rm -rf /mnt/purestorage1/Sequential-Write.0.0
 done
