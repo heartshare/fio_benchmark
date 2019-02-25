b1bb6aa58178adb15f1dc35cb7ae4e8f4fabedb7
KaitoAkita <kaitokun07@icloud.com>
2019-02-26 08:48:40 +0900

    全てrandom_readになっていたので修正

--- fio_shell/random_read/benchmark.sh
+++ fio_shell/random_read/benchmark.sh
@@ -1,30 +1,30 @@
 for i in `seq -w 10`
 do
-fio myjob.fio >> /root/random_read/1k/randrd_1k_$i.txt
-sed -i "s/bs=1k/bs=10k/g" myjob.fio
+fio /root/fio_shell/random_read/myjob.fio >> /root/random_read/1k/randrd_1k_$i.txt
+sed -i "s/bs=1k/bs=10k/g" /root/fio_shell/random_read/myjob.fio
 rm -rf /mnt/purestorage/Random-Read.0.0
 
-fio myjob.fio >> /root/random_read/10k/randrd_10k_$i.txt
-sed -i "s/bs=10k/bs=100k/g" myjob.fio
+fio /root/fio_shell/random_read/myjob.fio >> /root/random_read/10k/randrd_10k_$i.txt
+sed -i "s/bs=10k/bs=100k/g" /root/fio_shell/random_read/myjob.fio
 rm -rf /mnt/purestorage/Random-Read.0.0
 
-fio myjob.fio >> /root/random_read/100k/randrd_100k_$i.txt
-sed -i "s/bs=100k/bs=1m/g" myjob.fio
+fio /root/fio_shell/random_read/myjob.fio >> /root/random_read/100k/randrd_100k_$i.txt
+sed -i "s/bs=100k/bs=1m/g" /root/fio_shell/random_read/myjob.fio
 rm -rf /mnt/purestorage/Random-Read.0.0
 
-fio myjob.fio >> /root/random_read/1m/randrd_1m_$i.txt
-sed -i "s/bs=1m/bs=10m/g" myjob.fio
+fio /root/fio_shell/random_read/myjob.fio >> /root/random_read/1m/randrd_1m_$i.txt
+sed -i "s/bs=1m/bs=10m/g" /root/fio_shell/random_read/myjob.fio
 rm -rf /mnt/purestorage/Random-Read.0.0
 
-fio myjob.fio >> /root/random_read/10m/randrd_10m_$i.txt
-sed -i "s/bs=10m/bs=100m/g" myjob.fio
+fio /root/fio_shell/random_read/myjob.fio >> /root/random_read/10m/randrd_10m_$i.txt
+sed -i "s/bs=10m/bs=100m/g" /root/fio_shell/random_read/myjob.fio
 rm -rf /mnt/purestorage/Random-Read.0.0
 
-fio myjob.fio >> /root/random_read/100m/randrd_100m_$i.txt
-sed -i "s/bs=100m/bs=1g/g" myjob.fio
+fio /root/fio_shell/random_read/myjob.fio >> /root/random_read/100m/randrd_100m_$i.txt
+sed -i "s/bs=100m/bs=1g/g" /root/fio_shell/random_read/myjob.fio
 rm -rf /mnt/purestorage/Random-Read.0.0
 
-fio myjob.fio >> /root/random_read/1g/randrd_1g_$i.txt
-sed -i "s/bs=1g/bs=1k/g" myjob.fio
+fio /root/fio_shell/random_read/myjob.fio >> /root/random_read/1g/randrd_1g_$i.txt
+sed -i "s/bs=1g/bs=1k/g" /root/fio_shell/random_read/myjob.fio
 rm -rf /mnt/purestorage/Random-Read.0.0
 done
--- fio_shell/sequential_read/benchmark.sh
+++ fio_shell/sequential_read/benchmark.sh
@@ -1,30 +1,30 @@
 for i in `seq -w 10`
 do
-fio myjob.fio >> /root/sequential_read/1k/sequenrd_1k_$i.txt
-sed -i "s/bs=1k/bs=10k/g" myjob.fio
+fio /root/fio_shell/sequential_read/myjob.fio >> /root/sequential_read/1k/sequenrd_1k_$i.txt
+sed -i "s/bs=1k/bs=10k/g" /root/fio_shell/sequential_read/myjob.fio
 rm -rf /mnt/purestorage/Sequential-Read.0.0
 
-fio myjob.fio >> /root/sequential_read/10k/sequenrd_10k_$i.txt
-sed -i "s/bs=10k/bs=100k/g" myjob.fio
+fio /root/fio_shell/sequential_read/myjob.fio >> /root/sequential_read/10k/sequenrd_10k_$i.txt
+sed -i "s/bs=10k/bs=100k/g" /root/fio_shell/sequential_read/myjob.fio
 rm -rf /mnt/purestorage/Sequential-Read.0.0
 
-fio myjob.fio >> /root/sequential_read/100k/sequenrd_100k_$i.txt
-sed -i "s/bs=100k/bs=1m/g" myjob.fio
+fio /root/fio_shell/sequential_read/myjob.fio >> /root/sequential_read/100k/sequenrd_100k_$i.txt
+sed -i "s/bs=100k/bs=1m/g" /root/fio_shell/sequential_read/myjob.fio
 rm -rf /mnt/purestorage/Sequential-Read.0.0
 
-fio myjob.fio >> /root/sequential_read/1m/sequenrd_1m_$i.txt
-sed -i "s/bs=1m/bs=10m/g" myjob.fio
+fio /root/fio_shell/sequential_read/myjob.fio >> /root/sequential_read/1m/sequenrd_1m_$i.txt
+sed -i "s/bs=1m/bs=10m/g" /root/fio_shell/sequential_read/myjob.fio
 rm -rf /mnt/purestorage/Sequential-Read.0.0
 
-fio myjob.fio >> /root/sequential_read/10m/sequenrd_10m_$i.txt
-sed -i "s/bs=10m/bs=100m/g" myjob.fio
+fio /root/fio_shell/sequential_read/myjob.fio >> /root/sequential_read/10m/sequenrd_10m_$i.txt
+sed -i "s/bs=10m/bs=100m/g" /root/fio_shell/sequential_read/myjob.fio
 rm -rf /mnt/purestorage/Sequential-Read.0.0
 
-fio myjob.fio >> /root/sequential_read/100m/sequenrd_100m_$i.txt
-sed -i "s/bs=100m/bs=1g/g" myjob.fio
+fio /root/fio_shell/sequential_read/myjob.fio >> /root/sequential_read/100m/sequenrd_100m_$i.txt
+sed -i "s/bs=100m/bs=1g/g" /root/fio_shell/sequential_read/myjob.fio
 rm -rf /mnt/purestorage/Sequential-Read.0.0
 
-fio myjob.fio >> /root/sequential_read/1g/sequenrd_1g_$i.txt
-sed -i "s/bs=1g/bs=1k/g" myjob.fio
+fio /root/fio_shell/sequential_read/myjob.fio >> /root/sequential_read/1g/sequenrd_1g_$i.txt
+sed -i "s/bs=1g/bs=1k/g" /root/fio_shell/sequential_read/myjob.fio
 rm -rf /mnt/purestorage/Sequential-Read.0.0
 done
